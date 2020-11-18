1fa49e607b8c274d1070077ac40db5779de7af3f
nishikori0120 <yu.nishikori@di-v.co.jp>
2020-11-18 09:07:19 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,6 @@ class Room < ApplicationRecord
   # has_many :messages
   has_many :room_users
   has_many :users, through: :room_users
-
+  has_many :messages
   validates :name, presence: true
 end
